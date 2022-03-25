26e92259b48fd2f22a00bfe4ee160228d2586173
JoseER211 <88160386+JoseER211@users.noreply.github.com>
2022-03-25 16:31:41 -0500

    Se agregó contenido a la clase Song

---
+++ src/main/java/Spotify/Main.java
@@ -0,0 +1,6 @@
+package Spotify;
+
+public class Main {
+
+
+}
--- src/main/java/Spotify/Song.java
+++ src/main/java/Spotify/Song.java
@@ -1,4 +1,24 @@
 package Spotify;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class Song {
+    private String title;
+    private Integer id;
+    private Date date;
+    private Time duration;
+    private String genre;
+    private String mask;
+    private String description;
+
+    public Song(String title, Integer id, Date date, Time duration, String genre, String mask, String description) {
+        this.title = title;
+        this.id = id;
+        this.date = date;
+        this.duration = duration;
+        this.genre = genre;
+        this.mask = mask;
+        this.description = description;
+    }
 }
